43244e864effdd42db7c9a2ded0534bc28489d97
emna kaaniche <emnakaa19@gmail.com>
2024-05-01 02:06:53 -1200

    commit 7 	modified:   app.py 	modified:   assets/css/style1.css 	modified:   assets/css/templatemo-training-studio.css 	modified:   assets/images/etape1.png 	modified:   assets/images/etape2.png 	new file:   assets/images/logomini.png 	new file:   assets/js/formulaire.js 	modified:   assets/js/scriptuplode.js 	modified:   templates/index.html 	modified:   templates/login.html 	modified:   templates/signup.html 	modified:   templates/uplode.html

--- assets/js/scriptuplode.js
+++ assets/js/scriptuplode.js
@@ -19,3 +19,45 @@ fileInput.addEventListener('change', (event) => {
     loadingElement.style.display = 'none'; // Hide loading indicator when video is loaded
   });
 });
+
+
+const submitButton = document.getElementById('détection');
+const errorMessage = document.getElementById('error-message');
+
+// Allowed video formats (modify as needed)
+const allowedFormats = ['video/mp4', 'video/mov', 'video/webm'];
+
+fileInput.addEventListener('change', (event) => {
+  const selectedFile = event.target.files[0];
+
+  // Check if a file is selected
+  if (!selectedFile) {
+    errorMessage.textContent = 'Veuillez sélectionner un fichier.';
+    submitButton.disabled = true;
+    return;
+  }
+
+  // Check file type using MIME type
+  const fileType = selectedFile.type;
+  if (!allowedFormats.includes(fileType)) {
+    errorMessage.textContent = `Format de fichier non pris en charge. Veuillez sélectionner un fichier MP4, MOV ou WEBM.`;
+    submitButton.disabled = true;
+    return;
+  }
+ 
+
+  // File format is valid, enable submit button and clear error message
+  errorMessage.textContent = '';
+  submitButton.disabled = false;
+  
+});
+
+
+
+// Handle form submission (assuming further processing in another script)
+uploadForm.addEventListener('submit', (event) => {
+  // Prevent default form submission behavior
+  event.preventDefault();
+
+  // Handle video processing or upload logic here
+});
